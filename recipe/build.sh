e6601a81c5693472591228d9180869c19e29af9d
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 14:02:24 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/eclint -ldflags="-s -w" ./cmd/eclint
+go build -o=${PREFIX}/bin/eclint -ldflags="-s -w" ./cmd/eclint
 go-licenses save ./cmd/eclint --save_path=license-files
